e592d1df2a913d7785ec2a8b1c70d52becf268b2
BYWallace <bwallace90@gmail.com>
2014-05-27 16:54:15 -0400

    Basic listing done... working on getting grouped by date

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,11 @@ class WelcomeController < ApplicationController
 
   def index
     @events = Event.where(datetime_local: (Time.now.midnight..(Time.now.midnight + 7.day)))
+    @event_days = @events.group_by { |event| event.datetime_local.yday }
   end
+
+def archive
+
+end
 
 end
