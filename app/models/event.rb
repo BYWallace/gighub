42d4b257ee70aa7866ab3d376e0e316a426b2d7c
BYWallace <bwallace90@gmail.com>
2014-05-28 17:36:21 -0400

    Fixed validations for events.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,16 +1,15 @@
 class Event < ActiveRecord::Base
   belongs_to :venue
 
-  # validates :seatgeek_id, uniqueness: true
-  # validates :title, presence: true
-  # validates :datetime_local, presence: true
-  # validates :popularity, presence: true
-  # validates :venue_id, presence: true
+  validates :seatgeek_id, uniqueness: true
+  validates :title, presence: true
+  validates :datetime_local, presence: true
+  validates :venue_id, presence: true
 
   def self.generate_list
     response = HTTParty.get("http://api.seatgeek.com/2/events?geoip=50.200.196.50&range=25mi&per_page=1000&type=concert")["events"]
     response.each do |event|
-      event = Event.new(
+      new_event = Event.new(
         seatgeek_id: event["id"],
         title: event["title"],
         datetime_local: event["datetime_local"],
@@ -18,8 +17,8 @@ class Event < ActiveRecord::Base
         popularity: event["score"],
         venue_id: Venue.find_by(seatgeek_id: event["venue"]["id"]).id
       )
-      # event.venue = Venue.find_by(seatgeek_id: event["venue"]["id"]) if event["venue"]
-      # event.save
+      new_event.venue = Venue.find_by(seatgeek_id: event["venue"]["id"]) if event["venue"]
+      new_event.save
     end
   end
 end
