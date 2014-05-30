4e35f7f8f8990457fbca41fbc843f932c6075523
BYWallace <bwallace90@gmail.com>
2014-05-29 23:53:01 -0400

    Used includes to squeeze more performance and reduce calls to db

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
 
   def index
-    @events = current_user.events
+    @events = current_user.events.includes(:venue)
     @events_grouped = @events.group_by { |event| event.datetime_local.strftime("%A, %B #{event.datetime_local.day.ordinalize}") }
   end
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    @events = Event.where(datetime_local: (Time.now.midnight..(Time.now.midnight + 90.day)))
+    @events = Event.includes(:venue).where(datetime_local: (Time.now.midnight..(Time.now.midnight + 90.day)))
     @events_grouped = @events.group_by { |event| event.datetime_local.strftime("%A, %B #{event.datetime_local.day.ordinalize}") }
 
   end
